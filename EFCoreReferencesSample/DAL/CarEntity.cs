301288f114597fcb7b5b54f24cd6c878d24d17ec
Vladislav Shatravin <vladislav.shatravin@rubius.com>
2019-07-24 01:11:32 +0700

    Added segregated DDD model and entity

---
+++ EFCoreReferencesSample/DAL/CarEntity.cs
@@ -0,0 +1,15 @@
+﻿namespace EFCoreReferencesSample.DAL
+{
+    public class CarEntity
+    {
+        protected CarEntity()
+        {
+        }
+
+        public int Id { get; private set; }
+
+        public string LicensePlate { get; protected set; }
+
+        public int CarAccidents { get; protected set; }
+    }
+}
---
+++ EFCoreReferencesSample/Models/Car.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using EFCoreReferencesSample.DAL;
+
+namespace EFCoreReferencesSample.Models
+{
+    public class Car : CarEntity
+    {
+        private const int LicensePlateLength = 8;
+
+        public Car(string licensePlate, int carAccidents)
+        {
+            UpdateLicensePlate(licensePlate);
+            UpdateCarAccidentsCount(carAccidents);
+        }
+
+        //EF requires
+        private Car() { }
+
+        public void UpdateLicensePlate(string licensePlate)
+        {
+            if (licensePlate.Length != LicensePlateLength)
+            {
+                throw new Exception();
+            }
+
+            LicensePlate = licensePlate;
+        }
+
+        public void UpdateCarAccidentsCount(int carAccidents)
+        {
+            //assume error impossible
+            if (carAccidents < CarAccidents)
+            {
+                throw new Exception();
+            }
+
+            CarAccidents = carAccidents;
+        }
+    }
+}
